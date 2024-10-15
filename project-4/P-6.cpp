6b4e9dd247d16dd00d77b3e29a13de92578da5bd
savanpanchani <savanpanchani18@gmail.com>
2024-10-15 18:16:20 +0530

    Add files via upload

---
+++ project-4/P-6.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+	int i,j,s;
+	
+	for(i=1;i<=5;i++)
+	{
+		for(j=1;j<=i;j++)
+		{
+			printf("%d ",j);
+		}
+		for(s=5;s>i;s--)
+		{
+			printf("    ");
+		}
+		for(j=i;j>=1;j--)
+		{
+			printf("%d ",j);
+		}
+		printf("\n");
+	}
+}
